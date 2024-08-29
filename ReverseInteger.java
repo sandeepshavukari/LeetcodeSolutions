0a4da36dff94e1aa63d0a067b0ccbe03f72a243e
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-08-29 16:07:31 +0530

    ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -28,3 +28,12 @@ class Solution {
         return c;
     }
 }
+/*firtsly check that the number is +ve or negetive 
+if the num is positive as we know the logic similar to the palindrome 
+place that logic here then we can afford the reverse of the num 
+in the similar way if the number is -ve multiply it with -1 
+and do the same process again then we can get the reverse of the number
+Whereas if it is maxvalu of the integer type then we have to make it zero
+so every time when we check for the reminder of the number we have to check c
+is reched the max limit of integer or not 
+as we are chnaging the sign we have to consider only the max value only*/
