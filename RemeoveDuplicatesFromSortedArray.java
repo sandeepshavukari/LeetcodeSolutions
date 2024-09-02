2b713d1dc79eaad1054d0b322245df3b6f4c2088
sandeepshavukari <115909841+sandeepshavukari@users.noreply.github.com>
2024-09-02 10:55:38 +0530

    RemeoveDuplicatesFromSortedArray.java

--- RemeoveDuplicatesFromSortedArray.java
+++ RemeoveDuplicatesFromSortedArray.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         int a=0;
@@ -15,7 +16,20 @@ class Solution {
     }
 }
 
-/*The function removeDuplicates will update the input array nums to remove duplicates in-place.
-The function uses a position, a, of the last unique element. Next, it iterates through array nums. 
-Every time a new unique element (nums[i]) is found, the function will place it at the next position (nums[a + 1]). 
-At the end, the function will return a + 1, which represents the number of unique elements until that time through the modified array.*/
+/*# Approach
+Iterating the loop till the length if the array, while iterating the loop checking
+whether the prsent element and the next element are same or not so that i initialized
+a variable 'a' with 0, the a tracks the position of unique element in the array as the 
+first iteration is checking nums[0] with nums[0] it never goes to the if condition then
+the i is incremented and in the second iteration nums[0] is compared with nums[1] if again 
+same number i increments a dont if not the a increments then when the nums are nort same we have to make 
+change in the nums array by placing the unique elements one side by giving the nums[i] value to nums[a], 
+at last we rturn a+1 because the first element is also to be cnsidered then the 
+output is the elements that prints upto the retutned element size
+
+# Complexity
+- Time complexity:
+O(n)
+- Space complexity:
+O(1)
+*/
