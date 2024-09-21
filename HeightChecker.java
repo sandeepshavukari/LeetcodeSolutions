06221d71e34d752deecebd37479f69ba4b79d39b
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2024-09-21 09:56:15 +0530

    HeightsChecker

---
+++ HeightChecker.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int ar[]= new int[heights.length];
+        int c=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            ar[i]=heights[i];
+        }
+        Arrays.sort(heights);
+        for(int i=0;i<heights.length;i++)
+        {
+           if (ar[i]!=heights[i])
+           c++;
+        }
+        return c;
+    }
+}
