3405d7ace7c6e57db3d5d36f31b69e1335cb60ad
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2024-10-17 23:28:47 +0530

    Create RemeoveElement.java

---
+++ .vscode/RemeoveElement.java
@@ -0,0 +1,16 @@
+public class RemeoveElement { 
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int j = n-1, i = n-1, k = 0;
+        for(; i>=0; i--){
+            if(nums[i] == val){
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t; 
+                k++;
+                j--;
+            }
+        }
+        return n-k;
+    } 
+}
