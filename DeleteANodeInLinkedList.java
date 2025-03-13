291d20599ae5bc93e25bc712791876d9c5f447d5
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-03-13 23:54:27 +0530

    Create DeleteANodeInLinkedList.java

---
+++ DeleteANodeInLinkedList.java
@@ -0,0 +1,8 @@
+public class DeleteANodeInLinkedList {
+    public void deleteNode(ListNode node) {
+        if (node == null || node.next == null)
+            return; 
+        node.val = node.next.val; 
+        node.next = node.next.next; 
+    }
+}
