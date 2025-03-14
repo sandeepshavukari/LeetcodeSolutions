0eb300fe506f044ff29363f99c800835c3e9e129
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-03-14 19:29:31 +0530

    Create SerchInRotatedSortedArray.java

---
+++ SerchInRotatedSortedArray.java
@@ -0,0 +1,10 @@
+public class SerchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            return i;
+        }
+        return -1;
+    }
+}
