6dfbf4ec2533c931de77d97b0eabe78ff41adc43
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-03-14 20:02:23 +0530

    Update SerchInRotatedSortedArray.java

--- SerchInRotatedSortedArray.java
+++ SerchInRotatedSortedArray.java
@@ -1,4 +1,5 @@
 public class SerchInRotatedSortedArray {
+    //solution using linear serch
     public int search(int[] nums, int target) {
         for(int i=0;i<nums.length;i++)
         {
@@ -7,4 +8,28 @@ public class SerchInRotatedSortedArray {
         }
         return -1;
     }
+
+    //solution using binary serch
+    int l=0,h=ar.length-1;
+    while(l<=h)
+    {
+        int m=(l+h)/2;
+        if(ar[m]==key)
+        return m;
+        if(ar[m]>=ar[l])
+        {
+            if(ar[m]>=key&&ar[l]<=key)
+            h=m-1;
+            else
+            l=m+1;
+        }
+        else
+        {
+            if(ar[m]<=key&&ar[h]>=key)
+            l=m+1;
+            else
+            h=m-1;
+        }
+    }
+    return -1;
 }
