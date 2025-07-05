9149f43e4fbc57d9c20170860b5470145ff1b726
SHAVUKARI SANDEEP <2200032015@kluniversity.in>
2025-07-05 09:49:55 +0530

    ContainsDuplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s= new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            s.add(nums[i]);
+        } 
+        if(s.size()<nums.length)
+        return true;
+        return false;
+    }
+}
