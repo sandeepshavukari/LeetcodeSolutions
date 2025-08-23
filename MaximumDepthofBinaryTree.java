26370ed083982b137625f0aec77ebe9c66861849
sandeepshavukari <sandy20499@gmail.com>
2025-08-23 23:27:20 +0530

    MaximumDepthofBinaryTree

---
+++ MaximumDepthofBinaryTree.java
@@ -0,0 +1,8 @@
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int l=maxDepth(root.left);
+        int r=maxDepth(root.right);
+        return 1+Math.max(l,r);
+    }
+}
