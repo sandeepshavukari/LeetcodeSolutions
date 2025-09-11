00e8ddfb3b9bd5ac158c2ba6fb77a8d105a10578
sandeepshavukari <sandy20499@gmail.com>
2025-09-11 22:56:49 +0530

    WaterandJugProblem

---
+++ WaterandJugProblem.java
@@ -0,0 +1,13 @@
+public class WaterandJugProblem {
+
+    public boolean canMeasureWater(int x, int y, int target) {
+        if(x+y <target) return false;
+        int k=gcd(x,y);
+        if(target%k==0) return true;
+        return false;
+    }
+    public int gcd(int a,int b){
+         if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
