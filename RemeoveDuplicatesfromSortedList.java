5d1b89c4e71eca79cf1ea5ece81e6b7d68f688eb
SHAVUKARI SANDEEP <115909841+sandeepshavukari@users.noreply.github.com>
2025-09-12 23:46:56 +0530

    Update RemeoveDuplicatesfromSortedList.java

--- RemeoveDuplicatesfromSortedList.java
+++ RemeoveDuplicatesfromSortedList.java
@@ -4,9 +4,7 @@ import java.util.Set;
 class ListNode {
       int val;
       ListNode next;
-      ListNode() {
-            
-      }
+      ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
