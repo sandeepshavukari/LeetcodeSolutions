f942dd5ae0efa5573af22e562da63f4b58970fef
SHAVUKARI SANDEEP <115909841+sandeepshavukari@users.noreply.github.com>
2025-10-31 23:47:11 +0530

    Update MinAddToMakeValid.java

--- MinAddToMakeValid.java
+++ MinAddToMakeValid.java
@@ -1,1 +1,18 @@
-
+public class MinAddToMakeValid{
+  public int minAddToMakeValid(String s) {
+        int opb = 0;
+        int clb = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                opb++;
+            } else {
+                if (opb > 0) {
+                    opb--;
+                } else {
+                    clb++;
+                }
+            }
+        }
+          return opb + clb;
+    }
+}
