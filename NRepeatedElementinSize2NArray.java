72d11945bc9f1db54bcc92bbaca94afb7d6a9539
SHAVUKARI SANDEEP <115909841+sandeepshavukari@users.noreply.github.com>
2026-01-02 23:48:39 +0530

    Create NRepeatedElementinSize2NArray.java

---
+++ NRepeatedElementinSize2NArray.java
@@ -0,0 +1,9 @@
+class NRepeatedElementinSize2NArray {
+    public int repeatedNTimes(int[] nums) {
+        if(nums[0]==nums[nums.length-1])    return nums[0];
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]||nums[i]==nums[i+2])  return nums[i];
+        }
+        return nums[0];
+    }
+}
