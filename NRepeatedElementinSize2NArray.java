0b11c618460556b715a1d93f6c741a4600f2f555
SHAVUKARI SANDEEP <115909841+sandeepshavukari@users.noreply.github.com>
2026-01-05 23:26:06 +0530

    Update NRepeatedElementinSize2NArray.java

--- NRepeatedElementinSize2NArray.java
+++ NRepeatedElementinSize2NArray.java
@@ -4,7 +4,6 @@ class NRepeatedElementinSize2NArray {
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]||nums[i]==nums[i+2])  return nums[i];
         }
-        
         return nums[0];
     }
 }
