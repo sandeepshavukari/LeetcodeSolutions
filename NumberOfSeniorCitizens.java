9ed01c7b3680af58ca738a8eb5556a6a7a95e6e7
SHAVUKARI SANDEEP <115909841+sandeepshavukari@users.noreply.github.com>
2026-01-13 21:29:21 +0530

    Update NumberOfSeniorCitizens.java

--- NumberOfSeniorCitizens.java
+++ NumberOfSeniorCitizens.java
@@ -2,7 +2,7 @@ public NumberOfSeniorCitizens() {
     public int countSeniors(String[] details) {
         int c=0,n=details.length;
         for(int i=0;i<n;i++)
-        {
+        { 
            int a=Integer.parseInt(details[i].substring(11,13));
             if(a >60)
            c++;
